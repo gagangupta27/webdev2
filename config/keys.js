34ff04350b65ade0d57961402af86e4d18793ff1
gagangupta27 <43778554+gagangupta27@users.noreply.github.com>
2020-05-17 18:39:24 +0530

    del

--- config/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    mongodb:{
-        url:'mongodb+srv://gagangupta27:gagan717114@gagan-4szys.mongodb.net/test?retryWrites=true&w=majority'
-    },
-    sendgrid:{
-        key:'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'
-    }
-}
